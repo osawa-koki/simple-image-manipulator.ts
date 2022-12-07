158027e282561152d38022b06f504b6b4de0ded0
osawa-koki <github@osawa.dev>
2022-12-07 21:03:29 +0900

    表示用キャンバス。

--- src/Pages/Resize.tsx
+++ src/Pages/Resize.tsx
@@ -31,11 +31,35 @@ class Resize extends React.Component {
     await this.File2Jimp(file).then((image: Jimp): void => {
       this.image = image;
     });
-    console.log(this.image);
+    this.Draw();
   };
 
-  componentDidMount(): void {
-    //
+  Draw = (): void => {
+    if (this.image === null) {
+      return;
+    }
+    const canvas = this.CanvasRef.current;
+    if (canvas === null) {
+      return;
+    }
+    const ctx = canvas.getContext('2d');
+    if (ctx === null) {
+      return;
+    }
+    const width = this.image.bitmap.width;
+    const height = this.image.bitmap.height;
+    canvas.width = width;
+    canvas.height = height;
+    const imageData = ctx.createImageData(width, height);
+    imageData.data.set(this.image.bitmap.data);
+    ctx.putImageData(imageData, 0, 0);
+  };
+
+  CanvasRef: React.RefObject<HTMLCanvasElement>;
+
+  constructor(props: any) {
+    super(props);
+    this.CanvasRef = React.createRef();
   }
 
   render(): React.ReactNode {
@@ -43,16 +67,21 @@ class Resize extends React.Component {
       <div id="Resize">
         <h1>リサイズ</h1>
         <p>画像をリサイズします。</p>
-        <Dropzone onDrop={(files: File[]) => {this.FileDropped(files)}}>
-          {({getRootProps, getInputProps}) => (
-            <section>
-              <div id="DropZone" {...getRootProps()}>
-                <input {...getInputProps()} />
-                <p>画像ファイルをドロップして下さい。</p>
-              </div>
-            </section>
-          )}
-        </Dropzone>
+        <div>
+          <Dropzone onDrop={(files: File[]) => {this.FileDropped(files)}}>
+            {({getRootProps, getInputProps}) => (
+              <section>
+                <div id="DropZone" {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <p>画像ファイルをドロップして下さい。</p>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+        </div>
+        <div>
+        <canvas id="SampleCanvas" ref={this.CanvasRef}></canvas>
+        </div>
       </div>
     );
   }
