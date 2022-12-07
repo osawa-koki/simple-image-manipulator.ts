e99d255d1f063e7d79eac5260732b50539e3215e
osawa-koki <github@osawa.dev>
2022-12-07 21:14:23 +0900

    テキスト修正。

--- src/Pages/Root.tsx
+++ src/Pages/Root.tsx
@@ -7,7 +7,7 @@ import './Root.scss';
 function Root() {
   return (
     <div id="Root">
-      <Link className='RootLink' to={`${Settings.ROOT_PATH}/editor`}>リサイズ</Link>
+      <Link className='RootLink' to={`${Settings.ROOT_PATH}/editor`}>画像編集</Link>
     </div>
   );
 }
