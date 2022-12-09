9ccff3edce19691df2d68597b1a340ff5679853c
osawa-koki <github@osawa.dev>
2022-12-09 19:59:06 +0900

    モジュール別ファイルに。

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -5,30 +5,15 @@ import Dropzone from 'react-dropzone';
 
 import './Editor.scss';
 
+import File2Jimp from './Common/File2Jimp';
+
 class Editor extends React.Component {
 
   image: Jimp | null = null;
 
-  File2Jimp = async (file: File): Promise<Jimp> => {
-    return await new Promise((resolve, reject): void => {
-      const reader = new FileReader();
-      reader.onload = (event: ProgressEvent<FileReader>): void => {
-        if (event.target === null) {
-          reject();
-          return;
-        }
-        const data = event.target.result as ArrayBuffer;
-        Jimp.read(data as Buffer).then((image: Jimp): void => {
-          resolve(image);
-        });
-      };
-      reader.readAsArrayBuffer(file);
-    });
-  };
-
   FileDropped = async (acceptedFiles: File[]): Promise<void> => {
     const file = acceptedFiles[0];
-    await this.File2Jimp(file).then((image: Jimp): void => {
+    await File2Jimp(file).then((image: Jimp): void => {
       this.image = image;
     });
     this.Draw();
